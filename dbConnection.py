1c5fac40c122b45947e1aa44f8fecbf0426b87b0
CHANG SUNG BEOM <iwsl1234@naver.com>
2021-07-17 19:33:00 +0900

    python to mysql connection

---
+++ dbConnection.py
@@ -0,0 +1,22 @@
+import pymysql
+
+
+# connection 정보
+conn = pymysql.connect(
+    host = 'localhost', # host name
+    user = 'root', # user name
+    password = 'A1b2c3d4e5!', # password
+    db='test',
+    charset = 'utf8'
+)
+cursor = conn.cursor()
+
+sql = "SELECT * FROM test1"
+cursor.execute(sql)
+res = cursor.fetchall()
+
+for data in res:
+        print(data) 
+
+conn.commit()
+conn.close()
