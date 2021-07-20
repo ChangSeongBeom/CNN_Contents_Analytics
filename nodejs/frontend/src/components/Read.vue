0f6af945596c437505446b076144bece52f57bd7
CHANG SUNG BEOM <iwsl1234@naver.com>
2021-07-20 18:13:18 +0900

    단일페이지 완성

---
+++ nodejs/frontend/src/components/Read.vue
@@ -0,0 +1,56 @@
+<template>
+   
+
+   <div>
+       <div class="menu">
+       <a>욜로로그 분석</a>
+       <a>자막(OCR)</a>
+       <a>STT</a>
+   </div>
+
+
+
+     <div id="divid" v-for="user in users">{{user.contents}}<img width="500px":src="user.img_url"></div>
+
+   </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default{
+    
+    name:'Read',
+    data(){
+        return{
+        
+            users: []
+        }
+    },
+   created(){
+       var vm=this;
+       axios.get('http://localhost:3000/main')
+       .then(function(response){
+           console.log(response)
+           vm.users=response.data
+       })
+       .catch(function(){
+            
+       })
+   }
+}
+</script>
+<style scoped>
+.menu{
+    background: darkslateblue;
+    padding: 15px;
+    border-radius: 5px;
+}
+.menu a{
+    color: white;
+    padding: 10px;
+}
+#divid{
+    font-size:30px;
+}
+
+</style>
