ea6fddf1bee96f98c6bf996aa30306939639fbb4
CHANG SUNG BEOM <iwsl1234@naver.com>
2021-07-23 10:43:50 +0900

    backup

--- nodejs/frontend/src/components/Detailview.vue
+++ nodejs/frontend/src/components/Detailview.vue
@@ -1,16 +1,21 @@
 <template>
 
 <!--a href="javascript:;" @click="fnView(`${user.id}`)">-->
-    <div>
-        <div>sadfsfa </div>
-      <div id="baa" v-for="list in lists" >사물명{{list.object}}탐지개수{{list.cnt}}정확도{{list.avgpercent}} </br>
+
+        <div>
+        <div>{{lists[0]}}</div>
+        <div><b-table striped hover :items="lists" :fields="fields"></b-table></div>
+        </div>
+     <!-- <div id="baa" v-for="list in lists" >사물명{{list.object}}탐지개수{{list.cnt}}정확도{{list.avgpercent}} </br>-->
     
-    </div>   
+   
 </template>
+
 
 <script>
 import axios from 'axios';
 
+// lists=data.cnt.sort((a,b)=>{return a.avgpercent-b.avgpercent})
 export default {
     name:'Detailview',
     computed: {
@@ -19,13 +24,26 @@ export default {
             return this.$route.params.contentid;
         }
     },
-    
     data(){
         return{
-
-            lists : []
+            fields: [
+                {
+                    key: 'object',
+                    label: '탐지사물'
+                },
+                {
+                    key: 'cnt',
+                    label: '탐지횟수'
+                },
+                {
+                    key: 'avgpercent',
+                    label: '평균정확도'
+                }
+            ],
+            lists: []
         }
     },
+    
     created(){
             var vm=this;
             axios.get("http://localhost:3000/detailview/"+this.$route.params.contentid)
--- nodejs/frontend/src/components/View.vue
+++ nodejs/frontend/src/components/View.vue
@@ -1,29 +1,61 @@
 <template>
-	
+<canvas class="statistics-charts-line" ref="lineChart" width="500" height="400"></canvas>
 </template>
 
 
 <script>
-import axios from 'axios';
+import Chart from "chart.js";
 export default{
-	created(){
-            var vm=this;
-            axios.get("http://localhost:3000/detailview/"+"1")
-             .then(function(response){
-				 
-           vm.lists=response.data
-
-       })
-             .catch(function(){
-                alert(error)
-            })
-        },
-		data(){
-        return{
-
-            lists : []
-        }
+	data() {
+    return {
+      Chart,
+    };
+  }, 
+  async created() {
+    // 필요 시 데이터 가져오는 로직
+    // 차트생성에 사용할 옵션과 데이터가 결정될 것이다.
+  },
+  async mounted() {
+    // 마운트 된 두 ref에 접근할 수 있으므로 마운트 뒤 차트를 그린다.
+    await this.drawChart();
+  },
+  methods: {
+    drawChart() {
+      const chartCtx = this.$refs.lineChart.getContext("2d");
+      console.log(chartCtx, "chartCtx?");
+      // context와 Chart.js객체, 데이터가 결정됐으니  그리기만 하면된다.
     },
+  }
+	// 	data(){
+		
+    //     return{
+			
+    //         lists : []
+    //     }
+    // },
+	// created(){
+    //         var vm=this;
+    //         axios.get("http://localhost:3000/detailview/"+"1")
+    //          .then(function(response){
+    //        console.log(response)
+    //        vm.lists=response.data
+
+	// 	   console.log(response.data.length);
+	// 	console.log(typeof(response.data[0].object));
+		 
+	// 	arr2=[];
+	// 	   for(var i=0;i<response.data.length;i++){
+	// 		   console.log("바보");
+	// 		//    arr2.push(response.data[i].object);
+	// 		//    console.log(arr2);
+	// 	   }
+	// 	   console.log(arr2);
+    //    })
+    //          .catch(function(){
+    //             alert(error)
+    //         })
+    //     }
+	
 	//data(){
 		// console.log(lists);
 	
